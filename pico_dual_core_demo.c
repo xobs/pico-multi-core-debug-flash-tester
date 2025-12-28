f93a4f5ef93f7257869e7128c669ef5487b97e08
Sean Cross <sean@xobs.io>
2025-12-28 13:49:51 +0100

    rp2350: fix core index printing
    Signed-off-by: Sean Cross <sean@xobs.io>

--- pico_dual_core_demo.c
+++ pico_dual_core_demo.c
@@ -16,18 +16,22 @@ static bool memory_clobbering = false;
 
 // Silly little function to print hex characters without
 // any fancy math beyond shifts.
-static void print_hex(uart_inst_t * uart_id, uint32_t hex) {
+static void print_hex(uart_inst_t *uart_id, uint32_t hex)
+{
     const char digits[] = "0123456789abcdef";
     int offset;
     bool seen = false;
 
-    for (offset = 7; offset >= 0; offset -= 1) {
-        int digit = ((hex&(0xff<<(offset*4)))>>(offset*4))&0xf;
-        if ((digit == 0) && (offset == 0)) {
+    for (offset = 7; offset >= 0; offset -= 1)
+    {
+        int digit = ((hex & (0xff << (offset * 4))) >> (offset * 4)) & 0xf;
+        if ((digit == 0) && (offset == 0))
+        {
             uart_putc(uart_id, '0');
             continue;
         }
-        if (((!seen) && (digit == 0))) {
+        if (((!seen) && (digit == 0)))
+        {
             continue;
         }
         seen = true;
@@ -41,24 +45,29 @@ static void print_hex(uart_inst_t * uart_id, uint32_t hex) {
 // fine since we don't actually use any interrupts.
 __attribute__((used, section(".ram_vector_table")))
 uint8_t DATA[256];
-static void clobber_memory(void) {
+static void clobber_memory(void)
+{
     memory_clobbering = true;
     uint8_t data_offset = 0;
-    while (1) {
+    while (1)
+    {
         DATA[data_offset++]++;
     }
 }
 
 // Constantly print out a number so we know the target is alive.
-static void print_loop(void) {
+static void print_loop(void)
+{
     int i = 0;
 
-    while (1) {
+    while (1)
+    {
         uart_puts(UART_ID, "Loop counter!  i: 0x");
         print_hex(UART_ID, i);
         uart_puts(UART_ID, "  Core: ");
-        uart_putc(UART_ID, '0' + *((volatile uint32_t *)SIO_CPUID_OFFSET));
-        if (memory_clobbering) {
+        uart_putc(UART_ID, '0' + get_core_num());
+        if (memory_clobbering)
+        {
             uart_puts(UART_ID, "  Memory is being clobbered");
         }
         uart_puts(UART_ID, "\r\n");
@@ -66,7 +75,8 @@ static void print_loop(void) {
     }
 }
 
-void main(void) {
+void main(void)
+{
     // Set up our UART with the required speed.
     uart_init(UART_ID, BAUD_RATE);
 
